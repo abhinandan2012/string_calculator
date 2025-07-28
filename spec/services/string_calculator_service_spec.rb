ab675b7c1daa6261a7f1636d63300cadc2cea61e
abhinandan <abhinandank@bitlasoft.com>
2025-07-29 00:24:33 +0530

    Test Case 4: handle any amount of numbers

--- spec/services/string_calculator_service_spec.rb
+++ spec/services/string_calculator_service_spec.rb
@@ -14,6 +14,10 @@ RSpec.describe StringCalculator do
       it "returns the sum of two numbers" do
         expect(StringCalculatorService.add("1,5")).to eq(6)
       end
+
+      it "handle any amount of numbers" do
+        expect(StringCalculatorService.add("1,2,3,4,5")).to eq(15)
+      end
     end
   end
 end
