9c4ef0a7119fbcc9fb79f92f3fcea3249e590e07
abhinandan <abhinandank@bitlasoft.com>
2025-07-29 00:43:57 +0530

    Test Case 7: raises an error for negative numbers

--- app/services/string_calculator_service.rb
+++ app/services/string_calculator_service.rb
@@ -1,15 +1,19 @@
 class StringCalculatorService
-  def self.add(numbers)
-    return 0 if numbers.empty?
-    return numbers.to_i if numbers.size == 1
+  def self.add(input)
+    return 0 if input.empty?
+    return input.to_i if input.size == 1
 
     delimiter = ","
-    if numbers.start_with?("//")
-      parts = numbers.split("\n", 2)
+    if input.start_with?("//")
+      parts = input.split("\n", 2)
       delimiter = Regexp.escape(parts[0][2..-1])
-      numbers = parts[1]
+      input = parts[1]
     end
 
-    numbers.split(/#{delimiter}|\n/).map(&:to_i).sum
+    numbers = input.split(/#{delimiter}|\n/).map(&:to_i)
+    negatives = numbers.select { |n| n.negative? }
+    raise "negative numbers not allowed: #{negatives.join(',')}" if negatives.any?
+
+    numbers.sum
   end
 end
