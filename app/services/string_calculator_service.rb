287cd8d3913409899ad8631a4fbeac489f537fbb
abhinandan <abhinandank@bitlasoft.com>
2025-07-29 00:16:56 +0530

    Test Case 2: Single Number

--- app/services/string_calculator_service.rb
+++ app/services/string_calculator_service.rb
@@ -1,5 +1,6 @@
 class StringCalculatorService
   def self.add(numbers)
     return 0 if numbers.empty?
+    return numbers.to_i if numbers.size == 1
   end
 end
--- spec/services/string_calculator_service_spec.rb
+++ spec/services/string_calculator_service_spec.rb
@@ -6,6 +6,10 @@ RSpec.describe StringCalculator do
       it "returns 0" do
         expect(StringCalculatorService.add("")).to eq(0)
       end
+
+      it "returns the number for a single number" do
+        expect(StringCalculatorService.add("1")).to eq(1)
+      end
     end
   end
 end
