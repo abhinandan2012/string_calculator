9c5a08188d356d04b1e4d69252de788f998218fd
abhinandan <abhinandank@bitlasoft.com>
2025-07-29 00:22:04 +0530

    Test Case 3: Multi Numbers

--- app/services/string_calculator_service.rb
+++ app/services/string_calculator_service.rb
@@ -2,5 +2,6 @@ class StringCalculatorService
   def self.add(numbers)
     return 0 if numbers.empty?
     return numbers.to_i if numbers.size == 1
+    numbers.split(",").map(&:to_i).sum
   end
 end
--- spec/services/string_calculator_service_spec.rb
+++ spec/services/string_calculator_service_spec.rb
@@ -10,6 +10,10 @@ RSpec.describe StringCalculator do
       it "returns the number for a single number" do
         expect(StringCalculatorService.add("1")).to eq(1)
       end
+
+      it "returns the sum of two numbers" do
+        expect(StringCalculatorService.add("1,5")).to eq(6)
+      end
     end
   end
 end
